8bebd7350a9e50f6dbb13fc533986d0cb9e8c0d0
221B427 <221B427@juet.co.in>
2025-01-16 15:00:55 +0530

    AP Lab 0

---
+++ LE0.2.java
@@ -0,0 +1,10 @@
+
+public class LE0.2
+{
+	public static void main(String[] args) {
+	    int x = Integer.parseInt(args[0]);
+	    for(int i = 0; i <= 10; i++){
+		System.out.println(x+" X "+i + " = "+ i*x);
+	    }
+	}
+}
